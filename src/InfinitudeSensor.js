ed9109d6f215564e6adf95c10956399b8eebe522
rcoletti116 <coletti.robert@gmail.com>
2021-07-07 12:54:39 -0400

    zone cleanup

--- src/InfinitudeSensor.js
+++ src/InfinitudeSensor.js
@@ -1,50 +1,40 @@
 let Characteristic, Service;
 
 module.exports = class InfinitudeSensor {
-  constructor(name, zoneId, client, log, platformAccessory, service, characteristic) {
+  constructor(name, client, log, config, platformAccessory, service, characteristic) {
     this.name = name;
-    this.zoneId = zoneId;
     this.client = client;
-    this.log = log;
+    this.log = log;;
+    this.config = config;
 
     Service = service;
     Characteristic = characteristic;
 
     this.initialize(platformAccessory.getService(Service.TemperatureSensor));
   }
-  
-  initialize(TemperatureSensorService) {
-   TemperatureSensorService.setCharacteristic(
-      Characteristic.TemperatureDisplayUnits,
-      Characteristic.TemperatureDisplayUnits.FAHRENHEIT
-    );
 
+  initialize(TemperatureSensorService) {
     TemperatureSensorService
       .getCharacteristic(Characteristic.CurrentTemperature)
       .on(
         'get',
-        function(callback) {
-          this.getCurrentOutdoorTemperature().then(function(currentTemperature) {
-          callback(null, currentTemperature);
-        });
-      }.bind(this)
-    );
+        function (callback) {
+          this.getCurrentOutdoorTemperature().then(function (currentTemperature) {
+            callback(null, currentTemperature);
+          });
+        }.bind(this)
+      );
   }
 
   getCurrentOutdoorTemperature() {
-    return this.client.getStatus().then(function(status) {
-      return parseFloat(status['oat']);
-    });
-  }
-
-  getZoneStatus() {
     return this.client.getStatus().then(
-      function(status) {
-        return status.zones.zone.find(zone => zone['id'] === this.zoneId);
+      function (status) {
+        return this.client.fahrenheitToCelsius(parseFloat(status['oat'][0]), this.client.getTemperatureScale());
       }.bind(this)
     );
   }
 
+};
   getZoneTarget() {
     return this.client.getSystems().then(
       function(systems) {
