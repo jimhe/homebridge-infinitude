03659f252c6fd8617a79683190b648516af2b895
rcoletti116 <coletti.robert@gmail.com>
2021-09-08 14:26:45 -0400

    bug fixes, refactoring, fan improvements

--- src/InfinitudeFan.js
+++ src/InfinitudeFan.js
@@ -3,10 +3,10 @@ let Characteristic, Service;
 module.exports = class InfinitudeFan {
   constructor(name, zoneId, client, log, config, platformAccessory, service, characteristic) {
     this.name = name;
-    this.zoneId = zoneId;
     this.client = client;
     this.log = log;
     this.config = config;
+    this.zoneId = zoneId;
 
     Service = service;
     Characteristic = characteristic;
@@ -15,17 +15,17 @@ module.exports = class InfinitudeFan {
   }
 
   initialize(FanService) {
-    FanService
-      .getCharacteristic(Characteristic.Active).on(
+    FanService.getCharacteristic(Characteristic.Active)
+      .on(
         'get',
-      function (callback) {
-        this.getActiveState().then(function (state) {
-            callback (null, state);
-        });
-      }.bind(this)
+        function (callback) {
+          this.getActiveState().then(function (state) {
+            callback(null, state);
+          });
+        }.bind(this)
       );
 
-      FanService.getCharacteristic(Characteristic.TargetFanState)
+    FanService.getCharacteristic(Characteristic.TargetFanState)
       .on(
         'get',
         function (callback) {
@@ -34,40 +34,83 @@ module.exports = class InfinitudeFan {
           });
         }.bind(this)
       );
+
+    FanService.getCharacteristic(Characteristic.CurrentFanState)
+      .on(
+        'get',
+        function (callback) {
+          this.getActiveState().then(function (currentFanState) {
+            callback(null, currentFanState);
+          });
+        }.bind(this)
+      );
   }
 
-	getActiveState() {
-        return this.getZoneStatus().then(function (status) {
-        switch (status['fan'][0]) {
+  getCurrentState() {
+    return this.getZoneStatus().then(function (status) {
+      switch (status['fan'][0]) {
+        case 'off':
+          return Characteristic.CurrentFanState.IDLE;
+        default:
+          return Characteristic.CurrentFanState.BLOWING_AIR;
+      }
+    }.bind(this));
+  }
+
+  getActiveState() {
+    return this.getZoneStatus().then(function (status) {
+      switch (status['fan'][0]) {
         case 'off':
           return Characteristic.Active.INACTIVE;
-        case 'med':
-          return Characteristic.Active.ACTIVE;
-}
-});
-}
-
-        getTargetFanState() { return this.getZoneStatus().then(function (status) {
-        switch (status['fan'][0]) {
-        case 'off':
-          return Characteristic.TargetFanState.OFF;
         default:
+          return Characteristic.Active.ACTIVE;
+      }
+    }.bind(this));
+  }
+
+  getTargetFanState() {
+    return this.getScheduledActivity().then(function (status) {
+      switch (status['fan'][0]) {
+        case 'auto':
           return Characteristic.TargetFanState.AUTO;
-}
-});
-}
-	getZoneStatus() {
+        default:
+          return Characteristic.TargetFanState.MANUAL;
+      }
+    }.bind(this));
+  }
+  getZoneStatus() {
     return this.client.getStatus().then(
       function (status) {
         return status['zones'][0]['zone'].find(zone => zone['id'] === this.zoneId);
       }.bind(this)
     );
   }
-  
-  getZoneTarget() {
-    return this.client.getSystems().then(
-      function(systems) {
-        return systems['system'][0]['config'][0]['zones'][0]['zone'].find(zone => zone['id'] === this.zoneId);
+
+  getScheduledActivity() {
+    return this.client.getSystem().then(
+      function (system) {
+        var localTime = system.status['localTime'][0].substring(0, 19);
+        var systemDate = new Date(localTime);
+        var dayOfWeek = systemDate.getDay();
+        var time = (systemDate.getHours() * 100) + systemDate.getMinutes();
+        var zoneConfig = system.config['zones'][0]['zone'].find(zone => zone['id'] === this.zoneId);
+        var activePeriods = zoneConfig['program'][0]['day'][dayOfWeek]['period'].filter(p => {
+          var timePieces = p['time'][0].split(':');
+          var periodTime = timePieces[0] * 100 + timePieces[1] * 1;
+          return p['enabled'][0] === 'on' && periodTime <= time
+        });
+
+        var activityName = 'home';
+        if (activePeriods.length > 0) {
+          activityName = activePeriods[activePeriods.length - 1]['activity'][0];
+        }
+        else {
+          var zoneStatus = system.status['zones'][0]['zone'].find(zone => zone['id'] === this.zoneId);
+          activityName = zoneStatus['currentActivity'][0];
+        }
+
+        var activity = zoneConfig['activities'][0]['activity'].find(act => act['id'] === activityName);
+        return activity;
       }.bind(this)
     );
   }
