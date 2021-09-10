3c53a314bd5212b8a0d0bc2165f97dacabb29751
rcoletti116 <coletti.robert@gmail.com>
2021-09-10 13:32:22 -0400

    fix currentstate

--- src/InfinitudeFan.js
+++ src/InfinitudeFan.js
@@ -39,7 +39,7 @@ module.exports = class InfinitudeFan {
       .on(
         'get',
         function (callback) {
-          this.getActiveState().then(function (currentFanState) {
+          this.getCurrentState().then(function (currentFanState) {
             callback(null, currentFanState);
           });
         }.bind(this)
