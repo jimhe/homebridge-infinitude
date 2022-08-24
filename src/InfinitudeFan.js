f2fd9b20ee969b16a44a52c1eeaf33f8ad5bf8cd
dotfortun3 <trivialpendulum@protonmail.com>
2022-08-24 16:54:28 -0400

    Squashed commit of the following:
    commit 7ac262039f2cf4c03ba056568a102dc7ddd56afa
    Author: rcoletti116 <coletti.robert@gmail.com>
    Date:   Wed Mar 30 11:38:18 2022 -0400
    
        Update README.md
    
    commit 3c53a314bd5212b8a0d0bc2165f97dacabb29751
    Author: rcoletti116 <coletti.robert@gmail.com>
    Date:   Fri Sep 10 13:32:22 2021 -0400
    
        fix currentstate
    
    commit c9992f83320476245f495bc59fa10a7f95c80028
    Author: rcoletti116 <coletti.robert@gmail.com>
    Date:   Thu Sep 9 09:40:25 2021 -0400
    
        Update README.md
    
    commit 722ad790b5eb3406b372d21f21015cfb5b3cfadb
    Author: rcoletti116 <coletti.robert@gmail.com>
    Date:   Thu Sep 9 09:27:28 2021 -0400
    
        Update README.md
    
    commit 961df219adc5a72a2ca9057de8b408fd3c8911b8
    Author: rcoletti116 <coletti.robert@gmail.com>
    Date:   Thu Sep 9 09:25:54 2021 -0400
    
        Update README.md
    
    commit 08ac710936c4e30017793e48113ca46369e1b393
    Author: rcoletti116 <coletti.robert@gmail.com>
    Date:   Thu Sep 9 08:38:59 2021 -0400
    
        fix configureFanAccessory
    
        fixed line 168
    
    commit 1854af12a6ac2eb54acd692703afaf8d205a9385
    Author: rcoletti116 <coletti.robert@gmail.com>
    Date:   Wed Sep 8 14:29:02 2021 -0400
    
        refactor
    
    commit c382dda8a2f7a3073b4401e198ea65555e294cb9
    Author: rcoletti116 <coletti.robert@gmail.com>
    Date:   Wed Sep 8 14:28:22 2021 -0400
    
        refactor
    
    commit e0b9e76c9a265fbce4645f362cd96a115617b660
    Author: rcoletti116 <coletti.robert@gmail.com>
    Date:   Wed Sep 8 14:27:28 2021 -0400
    
        refactoring, fan
    
    commit 03659f252c6fd8617a79683190b648516af2b895
    Author: rcoletti116 <coletti.robert@gmail.com>
    Date:   Wed Sep 8 14:26:45 2021 -0400
    
        bug fixes, refactoring, fan improvements

--- src/InfinitudeFan.js
+++ src/InfinitudeFan.js
@@ -15,72 +15,75 @@ module.exports = class InfinitudeFan {
   }
 
   initialize(FanService) {
-    FanService.getCharacteristic(Characteristic.Active)
-      .on(
-        'get',
-        function (callback) {
-          this.getActiveState().then(function (state) {
-            callback(null, state);
-          });
-        }.bind(this)
-      );
+    FanService.getCharacteristic(Characteristic.Active).on(
+      'get',
+      function(callback) {
+        this.getActiveState().then(function(state) {
+          callback(null, state);
+        });
+      }.bind(this)
+    );
 
-    FanService.getCharacteristic(Characteristic.TargetFanState)
-      .on(
-        'get',
-        function (callback) {
-          this.getTargetFanState().then(function (targetFanState) {
-            callback(null, targetFanState);
-          });
-        }.bind(this)
-      );
+    FanService.getCharacteristic(Characteristic.TargetFanState).on(
+      'get',
+      function(callback) {
+        this.getTargetFanState().then(function(targetFanState) {
+          callback(null, targetFanState);
+        });
+      }.bind(this)
+    );
 
-    FanService.getCharacteristic(Characteristic.CurrentFanState)
-      .on(
-        'get',
-        function (callback) {
-          this.getActiveState().then(function (currentFanState) {
-            callback(null, currentFanState);
-          });
-        }.bind(this)
-      );
+    FanService.getCharacteristic(Characteristic.CurrentFanState).on(
+      'get',
+      function(callback) {
+        this.getCurrentState().then(function(currentFanState) {
+          callback(null, currentFanState);
+        });
+      }.bind(this)
+    );
   }
 
   getCurrentState() {
-    return this.getZoneStatus().then(function (status) {
-      switch (status['fan'][0]) {
-        case 'off':
-          return Characteristic.CurrentFanState.IDLE;
-        default:
-          return Characteristic.CurrentFanState.BLOWING_AIR;
-      }
-    }.bind(this));
+    return this.getZoneStatus().then(
+      function(status) {
+        switch (status['fan'][0]) {
+          case 'off':
+            return Characteristic.CurrentFanState.IDLE;
+          default:
+            return Characteristic.CurrentFanState.BLOWING_AIR;
+        }
+      }.bind(this)
+    );
   }
 
   getActiveState() {
-    return this.getZoneStatus().then(function (status) {
-      switch (status['fan'][0]) {
-        case 'off':
-          return Characteristic.Active.INACTIVE;
-        default:
-          return Characteristic.Active.ACTIVE;
-      }
-    }.bind(this));
+    return this.getZoneStatus().then(
+      function(status) {
+        switch (status['fan'][0]) {
+          case 'off':
+            return Characteristic.Active.INACTIVE;
+          default:
+            return Characteristic.Active.ACTIVE;
+        }
+      }.bind(this)
+    );
   }
 
   getTargetFanState() {
-    return this.getScheduledActivity().then(function (status) {
-      switch (status['fan'][0]) {
-        case 'auto':
-          return Characteristic.TargetFanState.AUTO;
-        default:
-          return Characteristic.TargetFanState.MANUAL;
-      }
-    }.bind(this));
+    return this.getScheduledActivity().then(
+      function(status) {
+        switch (status['fan'][0]) {
+          case 'auto':
+            return Characteristic.TargetFanState.AUTO;
+          default:
+            return Characteristic.TargetFanState.MANUAL;
+        }
+      }.bind(this)
+    );
   }
   getZoneStatus() {
     return this.client.getStatus().then(
-      function (status) {
+      function(status) {
         return status['zones'][0]['zone'].find(zone => zone['id'] === this.zoneId);
       }.bind(this)
     );
@@ -88,23 +91,22 @@ module.exports = class InfinitudeFan {
 
   getScheduledActivity() {
     return this.client.getSystem().then(
-      function (system) {
+      function(system) {
         var localTime = system.status['localTime'][0].substring(0, 19);
         var systemDate = new Date(localTime);
         var dayOfWeek = systemDate.getDay();
-        var time = (systemDate.getHours() * 100) + systemDate.getMinutes();
+        var time = systemDate.getHours() * 100 + systemDate.getMinutes();
         var zoneConfig = system.config['zones'][0]['zone'].find(zone => zone['id'] === this.zoneId);
         var activePeriods = zoneConfig['program'][0]['day'][dayOfWeek]['period'].filter(p => {
           var timePieces = p['time'][0].split(':');
           var periodTime = timePieces[0] * 100 + timePieces[1] * 1;
-          return p['enabled'][0] === 'on' && periodTime <= time
+          return p['enabled'][0] === 'on' && periodTime <= time;
         });
 
         var activityName = 'home';
         if (activePeriods.length > 0) {
           activityName = activePeriods[activePeriods.length - 1]['activity'][0];
-        }
-        else {
+        } else {
           var zoneStatus = system.status['zones'][0]['zone'].find(zone => zone['id'] === this.zoneId);
           activityName = zoneStatus['currentActivity'][0];
         }
